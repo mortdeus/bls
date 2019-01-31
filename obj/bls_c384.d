18135af51fc6db6751bb7990db15f0511b9ed705
mortdeus <mortdeus@gmail.com>
2019-01-31 20:13:43 +0000

    fix

---
+++ obj/bls_c384.d
@@ -0,0 +1,82 @@
+obj/bls_c384.o: src/bls_c384.cpp src/bls_c_impl.hpp include/bls/bls.h \
+ /home/mortdeus/work/bls/../mcl/include/mcl/bn.h \
+ /home/mortdeus/work/bls/../mcl/include/mcl/curve_type.h \
+ /home/mortdeus/work/bls/../mcl/include/mcl/impl/bn_c_impl.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/bn384.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/bn.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/fp_tower.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/fp.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/hash.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/inttype.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/stream.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/exception.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/op.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/gmp_util.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/randgen.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/random_generator.hpp \
+ /home/mortdeus/include/gmpxx.h /home/mortdeus/include/gmp.h \
+ /home/mortdeus/work/bls/../mcl/include/mcl/array.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/util.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/bit_operation.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/operator.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/conversion.hpp \
+ /home/mortdeus/work/bls/../mcl/include/cybozu/itoa.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/ec.hpp \
+ /home/mortdeus/work/bls/../mcl/include/mcl/lagrange.hpp \
+ src/./qcoeff-bn254.hpp
+
+src/bls_c_impl.hpp:
+
+include/bls/bls.h:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/bn.h:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/curve_type.h:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/impl/bn_c_impl.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/bn384.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/bn.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/fp_tower.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/fp.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/hash.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/inttype.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/stream.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/exception.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/op.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/gmp_util.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/randgen.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/random_generator.hpp:
+
+/home/mortdeus/include/gmpxx.h:
+
+/home/mortdeus/include/gmp.h:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/array.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/util.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/bit_operation.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/operator.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/conversion.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/cybozu/itoa.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/ec.hpp:
+
+/home/mortdeus/work/bls/../mcl/include/mcl/lagrange.hpp:
+
+src/./qcoeff-bn254.hpp:
